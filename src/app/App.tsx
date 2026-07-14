4eaf79e83ba4390e9b72a39b6290a0a7d599a33b
Mudithawelivita <mudithawelivita@users.noreply.github.com>
2026-07-14 22:44:19 +0530

    Update latest video embed link to use watch URL format

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -921,7 +921,7 @@ export default function App() {
                   <div className="relative bg-black" style={{ paddingBottom: '56.25%' }}>
                     <iframe
                       className="absolute top-0 left-0 w-full h-full"
-                      src="https://www.youtube.com/embed/lAdlu0fQk8o"
+                      src="https://www.youtube.com/watch?v=lAdlu0fQk8o"
                       title="Latest Upload"
                       frameBorder="0"
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
